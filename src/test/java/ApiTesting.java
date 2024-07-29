0377eeba050656ef561605b3da21be7be43d49fc
raghulsusendran <raghulsusendran@gmail.com>
2024-07-29 21:39:46 +0530

    Rest assured has been added

--- src/test/java/ApiTesting.java
+++ src/test/java/ApiTesting.java
@@ -1,12 +1,25 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
-import io.restassured.response.*;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 public class ApiTesting {
 
 	@Test
 	public void testUser() {
-		
-		
-	}
-	
+		 RestAssured.baseURI = "https://fake-json-api.mock.beeceptor.com";
+		 
+		 Response response = RestAssured.given()
+		            .when().get("/users")
+		            .then()
+		            .statusCode(200)
+		            .extract()
+		            .response();
+		 System.out.println(response.asString());
+		 
+		 int length = response.jsonPath().getList("").size();
+		 
+		 Assert.assertEquals(length, 10);
+    }
 }
